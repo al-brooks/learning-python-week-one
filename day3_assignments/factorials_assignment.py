e80800789f7cca1f68babc36ad6e9f0dbe654b75
Alexander Brooks <alexanderlawbrooks@gmail.com>
2021-07-11 21:35:35 -0400

    updated factorial logic layout

--- day3_assignments/factorials_assignment.py
+++ day3_assignments/factorials_assignment.py
@@ -1,25 +1,30 @@
 # Assignment 1: Factorial
 def calculate_factorial(number):
-    # number_range = [] # Method 2
     factorial = 1
 
-    # while number > 0: # Method 2
+    for number in range(1, number + 1): # up to but not including (end number) - add 1
+        print(f"{factorial} is the factorial and will be multiplied by: {number}") # list out each loop iteration
+        factorial *= number
+    
+    print(f"The factorial value of {number} is: {factorial}")
+    return factorial
+# --------------------------------------------------------------
+    # Alternate method - using While loop and array
+
+    # number_range = []
+
+    # while number > 0:
     #     number_range.append(number)
     #     number -= 1
 
-
-    for number in range(1, number + 1): # up to but not including (end number) - add 1
-        print(f"{factorial} is the factorial and is being multiplied by: {number}") # list out each loop iteration
-        factorial *= number
-    
-    # for index in range(0, len(number_range)): # Method 2
+    # for index in range(0, len(number_range)):
     #     factorial *= number_range[index]
     #     # First Loop: Factorial = 1(original value) * 4
         # Second Loop: Factorial = 4(new value) * 3
         # etc.
-
-    return factorial
+# --------------------------------------------------------------
+    
 
 number = int(input("Enter a number: "))
 
-print(calculate_factorial(number))
+calculate_factorial(number)
